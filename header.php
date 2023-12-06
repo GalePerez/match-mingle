0c52b9bfb7a2533e8473cd8c9b59e88ed6ccd4f6
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-12-06 20:36:06 +0800

    Fix ui

--- header.php
+++ header.php
@@ -27,7 +27,7 @@ $userData['about_me'] = json_decode($userData['about_me'],true);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
     <link rel="stylesheet" href="<?php echo BASE_URL ?>style/sidebar.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>style/home.css">
+
 </head>
 <body>
     <nav class="sidebar close">
--- home.php
+++ home.php
@@ -1,40 +1,46 @@
 <?php include 'header.php'; ?>
-
+<link rel="stylesheet" href="<?php echo BASE_URL ?>style/home.css">
 <section class="home">
   <div class="text">Home</div>
   <div class="main-container">
     <div class="container-1">
       <a href="profile.php">
-        <div>
-          <img src="./src/home_photos/matchmake.png" style="height: 400px; width: 400px;" class="matchmake-img" />
-        </div>
-        <div>
-          <h1>Meet and Match!<h1>
+        <div class="match-box">
+          <div>
+            <img src="./src/home_photos/matchmake.png" style="height: 250px; width: 250px;" class="matchmake-img" />
+          </div>
+          <div>
+            <h1>Meet and Match!<h1>
+          </div>
         </div>
       </a>
     </div>
     <div class="container-2">
       <div class="container-3">
         <a href="profile.php">
-          <div>
-            <img src="./src/home_photos/viewprofile.png" style="height: 200px; width: 190px;">
-          </div>
-          <div>
-            <h3>View/Edit Profile<h3>
+          <div class="profile-box">
+            <div>
+              <img src="./src/home_photos/viewprofile.png" style="height: 100px; width: 100px;">
+            </div>
+            <div>
+              <h5>View/Edit Profile<h5>
+            </div>
           </div>
         </a>
       </div>
       <div class="container-4">
         <a href="profile.php">
-          <div>
-            <img src="./src/home_photos/chat.png" style="height: 220px; width: 210px;">
+          <div class="chat-box">
+            <div>
+                <img src="./src/home_photos/chat.png" style="height: 100px; width: 100px;">
+            </div>
+            <div>
+                <h5>Chat your Match!<h5>
+              </div>
+            </div>
           </div>
-          <div>
-            <h3>Chat your Match!<h3>
-          </div>
+        </a>
       </div>
-      </a>
-    </div>
   </div>
 </section>
 
